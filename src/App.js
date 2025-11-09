e26c30bbd7c90e9defde03fac6806d0483b0f0c5
dino saur <marcusgupta1@gmail.com>
2025-11-09 23:28:01 +0000

    Add live bus locations

--- src/App.js
+++ src/App.js
@@ -25,14 +25,16 @@ xhr.send();
 console.log("yo");
 //var m = xhr.send("GET /v1/live_bus_times/8")*/
 
+const BUS_STOPS = 'https://tfe-opendata.com/api/v1/stops'
 const LIVE_BUS = "https://tfe-opendata.com/api/v1/live_bus_times/";
+const BUS_POS = "https://tfe-opendata.com/api/v1/vehicle_locations"
 
 const COLOURS = {
   bus: "red",
   tram: "green"
 }
 
-const myIcon = (colour = "red") =>
+const stopIcon = (colour = "red") =>
   L.divIcon({
     className: "bus-stop",
     html: `<div style="
@@ -44,7 +46,7 @@ const myIcon = (colour = "red") =>
   });
 
 
-function Popp({content, routes, setRoutes}) {
+function PopupComponent({content, routes, fetchh, setPath}) {
   //console.log(typeof(content));
   if (typeof(content) == "string" || (content.hasOwnProperty('content'))) {
     //console.log("stringg");
@@ -53,24 +55,28 @@ function Popp({content, routes, setRoutes}) {
     );
   }
   else {
-    console.log("data to load");
+    //setPath([]);
+    //console.log(content[0].key);
+    console.log("toilet ohio rizz");
     //console.log(content);
     //console.log(typeof(content));
     return (
       <>{
       content.map((st) => {
+        //console.log(st.key);
         let timer = st.unixtime - Math.floor(Date.now() / 1000);
         timer = Math.round(timer/60);
         let timerOut = "";
         //console.log(typeof(timer));
-
+        //console.log(content[0].key);
         if (timer==1) {timerOut="in 1 min"}
         else if (timer==0) {timerOut="due"}
         else if (timer<0) {timerOut="overdue"}
         else {timerOut= ("in "+timer+" mins")}
         return (
           <div class="bus" style={{whiteSpace: "nowrap"}}>
-          <p style={{color: routes[st.route]? routes[st.route].textt : "white", backgroundColor: routes[st.route] ? routes[st.route].colour : "green", display:"inline", paddingLeft: "2px", paddingRight: "2px", marginTop: "2px", marginBottom: "2px"}}>
+            <button onClick={() => fetchh(st.route, setPath)}>Hey!</button>
+            <p style={{color: routes[st.route]? routes[st.route].textt : "white", backgroundColor: routes[st.route] ? routes[st.route].colour : "green", display:"inline", paddingLeft: "2px", paddingRight: "2px", marginTop: "2px", marginBottom: "2px"}}>
             {st.route}
             
             </p> to {st.dest} {timerOut}
@@ -83,20 +89,42 @@ function Popp({content, routes, setRoutes}) {
   }
 };
 
-function MyComponent() {
+function VehicleComponent({path}) {
+  if (path != []) {
+    return (
+      <>
+        {path.map((b) => {
+          console.log("hey");
+          return (
+            <Marker
+              position={[b.lat, b.lon]}
+              icon={stopIcon(b.colour)}
+            >
+
+            </Marker>
+          )}
+        )}
+      </>
+    )
+  }
+  else return (null);
+}
+
+function MarkerComponent() {
   const popupRef = useRef(null);
   const [popupContent, setPopupContent] = useState("Loading");
   const [centre, setCentre] = useState([55.95, -3.18]);
   const [zoom, setZoom] = useState(12);
   const [routes, setRoutes] = useState([]);
   const [stops, setStops] = useState([]); //this is to let stops be updatable
+  const [path, setPath] = useState([]);
   const mapp = useMap();
   //console.log(map.getZoom());
 
 
   useEffect(() => {
     //console.log("ftech");
-    fetch('https://tfe-opendata.com/api/v1/stops')
+    fetch(BUS_STOPS)
     .then((res) => res.json())
     .then((data) => {
         //console.log("data");
@@ -171,6 +199,7 @@ function MyComponent() {
       .then((data) => {
         if (data != null) {
           newContent = data.map((s) => ({
+            key: key,
             route: s.routeName,
             early: s.departures[0].departureTime,
             unixtime: s.departures[0].departureTimeUnix,
@@ -187,6 +216,101 @@ function MyComponent() {
 
   }
 
+  function fetchVehicles(key) {
+    console.log("fetching data");
+    fetch(BUS_POS)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data != null) {
+          const vehicles = data.vehicles.map((bus) => ({
+            id: bus.service_name,
+            lat: bus.latitude,
+            lon: bus.longitude,
+            color: "orange"
+          }));
+          const filteredVehicles = vehicles.filter((vehicle) => vehicle.id == key)
+          console.log(filteredVehicles.length);
+          setPath(filteredVehicles);
+        }
+        else {
+          console.log("Erm ackshually");
+        }
+      })
+  }
+  /*const fetchVehicles = async (key) => {
+    try {
+      console.log("fetching data")
+      console.log(key);
+      const res = await fetch(BUS_POS);
+      const data = await res.json();
+      console.log(data);
+        
+
+      if (data?.vehicles) {
+        // Filter based on the key (the bus stop ID) to fetch relevant vehicles
+        console.log(data.vehicles[0].service_name);
+        console.log(data.vehicles[1].service_name);
+        let filteredVehicles = [];
+        console.log(data.vehicles.length);
+
+        for (let i=0; i< data.vehicles.length; i++) {
+          let bus = data.vehicles[i];
+          //console.log(bus.service_name);
+          console.log(key);
+          if (bus.service_name==key) {
+            let entry = {
+              lat: bus.latitude,
+              lon: bus.longitude,
+              color: "orange"
+            }
+            filteredVehicles.push(entry);
+          }
+        }
+        console.log(filteredVehicles);
+        setPath(filteredVehicles);
+      }
+      else {
+        console.log("erm ackshually");
+      }
+    } catch (error) {
+      console.error("Error fetching vehicle locations:", error);
+    }
+  };*/
+
+
+  /*function fetchRoutes(route, setPath) {
+    console.log("Fetching routes!!!");
+    let word = BUS_POS;
+    let newData = [];
+    fetch(word)
+    .then((res) => res.json())
+    .then((data) => {
+      
+      if (data != null) {
+        console.log(data.vehicles);
+        //console.log(typeof(data.vehicles));
+        let listt = data.vehicles;
+        //console.log(typeof(listt));
+        for (let i=0; i< listt.length; i++) {
+          let bus = listt[i];
+          //console.log(bus.service_name);
+          if (bus.service_name==route) {
+            let entry = {
+              lat: bus.latitude,
+              lon: bus.longitude,
+              color: "orange"
+            }
+            newData.push(entry);
+          }
+        }
+      }
+      console.log("nd")
+      console.log(newData);
+      setPath(newData);
+    })
+    
+
+  }*/
 
   //console.log('map center:', map.getCenter());
   //const centre = mapp.getCenter();
@@ -196,38 +320,45 @@ function MyComponent() {
     //console.log("happy")
     return (
       <>
-        {stops.map((st) => {
-          //console.log(stops);
-          // Render the station
-          const latd = 0.010;
-          const lond = 0.030;
-          if (Math.abs(st.lat-centre.lat) <= latd && Math.abs(st.lon-centre.lng) <= lond) { 
-          return (
-            <Marker
-              key={st.id}
-              position={[st.lat, st.lon]}
-              icon={myIcon(st.type)}
-              eventHandlers={{
-                click: () => {
-                  handlePopupOpen(st.id);
-                },
-              }}
-            >
-              <Popup>
-                <strong>{st.name}</strong>
-                <Popp content={popupContent} routes={routes} setRoutes={setRoutes}/>
-              </Popup>
-            </Marker>
-          );
+        <VehicleComponent path={path}/>
+
+
+
+        {
+        
+        stops.map((st) => {
+            //console.log(stops);
+            // Render the station
+            const latd = 0.010;
+            const lond = 0.030;
+            if (Math.abs(st.lat-centre.lat) <= latd && Math.abs(st.lon-centre.lng) <= lond) { 
+            return (
+              <Marker
+                key={st.id}
+                position={[st.lat, st.lon]}
+                icon={stopIcon(st.type)}
+                eventHandlers={{
+                  click: () => {
+                    handlePopupOpen(st.id);
+                  },
+                }}
+              >
+                <Popup>
+                  <strong>{st.name}</strong>
+                  <PopupComponent content={popupContent} routes={routes} fetchh={fetchVehicles} setPath={setPath} />
+                </Popup>
+              </Marker>
+            );
         }
         return null;
-      })}
+      })
+      }
     </>
   );
   }
   else {
     //console.log("sad")
-    return null;
+    return <VehicleComponent path={path}/>;
   }
 }
 
@@ -265,7 +396,7 @@ export default function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <MyComponent />
+      <MarkerComponent />
       
       
     </MapContainer>
